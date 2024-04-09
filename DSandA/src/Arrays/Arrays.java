c6c310cc07fbe17c9287e39acf92e8bad2bf7d12
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-04-09 16:24:27 +0530

    Create normal Array

---
+++ DSandA/src/Arrays/Arrays.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class Arrays {
+    public static void normalArray (){
+        int[] numbersArray = new int[3];
+        numbersArray[0] = 10;
+        numbersArray[1] = 20;
+        numbersArray[2] = 30;
+
+        System.out.println(java.util.Arrays.toString(numbersArray));
+    }
+}
--- DSandA/src/Main.java
+++ DSandA/src/Main.java
@@ -1,15 +1,17 @@
 import BigO.BigO;
+import Arrays.Arrays;
 public class Main {
 
 
 
     public static void main(String[] args) {
-        int[] myNumbers = {1, 2, 3, 4, 5};
+        //int[] myNumbers = {1, 2, 3, 4, 5};
 
-        BigO.log(myNumbers);
-        BigO.logLoop(myNumbers);
-        BigO.logMore(myNumbers);
-        BigO.logMoreThanMore(myNumbers);
+        //BigO.log(myNumbers);
+        //BigO.logLoop(myNumbers);
+        //BigO.logMore(myNumbers);
+        //BigO.logMoreThanMore(myNumbers);
+        Arrays.normalArray();
 
 
     }
